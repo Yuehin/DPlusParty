3d5c67108bfe5f07e97a578104f92092073922f0
Aaron Chen <aaronjc12@gmail.com>
2020-09-29 10:51:23 -0700

    DPlus Party (v1.4.2)
    - Removed FB and Twitter Sharing
    - Added new brokering server

--- dplusparty.js
+++ dplusparty.js
@@ -303,7 +303,7 @@ function initialize(data) {
 
     // Create own peer object with connection to shared PeerJS server
     peer = new Peer(lastPeerId, {
-        host: 'dplus-2.uc.r.appspot.com',
+        host: 'erudite-marker-290616.uc.r.appspot.com',
         path: '/dplus',
         config: {'iceServers': servers},
         debug: 4
@@ -775,14 +775,6 @@ function injectChat() {
                 <button class="chat-btn dp-rh" id="emoji-button" title="Insert Emoticon">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/></svg>
                 </button>
-            </div>
-            <div style="text-align:right; margin: 0 5px 0 103px; display: flex;">
-                <div id="fb-root"></div>
-                <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v7.0&appId=1454127708223187&autoLogAppEvents=1" nonce="qpOjsjpv"></script>
-                <div class="fb-share-button" data-href="https://chrome.google.com/webstore/detail/dplus-party-watch-disney/ckhlohlbolmihakbnlddceackadnodoe" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fchrome.google.com%2Fwebstore%2Fdetail%2Fdplus-party-watch-disney%2Fckhlohlbolmihakbnlddceackadnodoe&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
-                <div>
-                    <a href="https://twitter.com/intent/tweet?button_hashtag=DPlusParty&ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-text="Lets watch Disney+ together! #DPlusParty" data-url="https://chrome.google.com/webstore/detail/dplus-party-watch-disney/ckhlohlbolmihakbnlddceackadnodoe" data-show-count="true"></a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                </div>
             </div>
             <div id="kofi" style="text-align:right; margin: 0 5px;">
                 <div class="btn-container">
